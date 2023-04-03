f7d6b3a719ee8e576c169a4afbb83cc6ba7d6fc0
unknown <nguyenthtam19@gmail.com>
2023-04-03 07:03:45 +0700

    first commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '04344cc5cemsh585a54243c4ae05p12e21djsna0277bc87d90',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5/";
+export const WEATHER_API_KEY = "0ff79c06b152c12b95bb45179c5746f3";
